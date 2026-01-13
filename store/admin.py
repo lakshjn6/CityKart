06cfd36d40a08f76b390778b831ef25c07fd8d74
Lakshay Mittal <lakshayjain0611@gmail.com>
2026-01-13 12:18:28 +0530

    first commit

---
+++ store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import product
+
+class productAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'is_available', 'category', 'created_date', 'modified_date')
+    prepopulated_fields = {'slug':('product_name',)}
+
+# Register your models here.
+admin.site.register(product, productAdmin)
